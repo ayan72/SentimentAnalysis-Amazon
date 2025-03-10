b16a85b31c08c6f7b38838340469a695dcf6fb8f
ayan72 <74502998+ayan72@users.noreply.github.com>
2025-03-10 17:45:27 -0600

    Compare the sentiment analysis of the two different models

--- sentiment.py
+++ sentiment.py
@@ -13,12 +13,15 @@ nltk.download('words')
 nltk.download('vader_lexicon')
 from nltk.sentiment import SentimentIntensityAnalyzer
 
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
+from scipy.special import softmax
 
 df = pd.read_csv("/Users/ayaan/Downloads/data-amazon/Reviews.csv")
 
-# print(df.head())
+print(df.head())
 
-# print(df.shape)
+print(df.shape)
 
 #EDA
 ax = df['Score'].value_counts().sort_index() \
@@ -26,7 +29,7 @@ ax = df['Score'].value_counts().sort_index() \
           title='Count of Reviews by Stars',
           figsize=(10, 5))
 ax.set_xlabel('Review Stars')
-# plt.show()
+plt.show()
 
 #NLTK
 example = df['Text'][50]
@@ -54,14 +57,64 @@ for i, row in df.iterrows():
     res[myid] = sia.polarity_scores(text)
 
 count = 0
-# for key, value in res.items():
-#     if count >= 3:  # Stop after 3
-#         break
-#     print(f"{key}: {value}")
-#     count += 1
 
 vaders = pd.DataFrame(res).T
 vaders = vaders.reset_index().rename(columns={'index': 'Id'})
 vaders = vaders.merge(df, how='left')
 
 print(vaders.head())
+ax = sns.barplot(data=vaders, x='Score', y='compound')
+ax.set_title('Compund Score by Amazon Star Review')
+plt.show()
+
+fig, axs = plt.subplots(1, 3, figsize=(12, 3))
+sns.barplot(data=vaders, x='Score', y='pos', ax=axs[0])
+sns.barplot(data=vaders, x='Score', y='neu', ax=axs[1])
+sns.barplot(data=vaders, x='Score', y='neg', ax=axs[2])
+axs[0].set_title('Positive')
+axs[1].set_title('Neutral')
+axs[2].set_title('Negative')
+plt.tight_layout()
+plt.show()
+
+MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+
+def polarity_scores_roberta(example):
+    encoded_text = tokenizer(example, return_tensors='pt')
+    output = model(**encoded_text)
+    scores = output[0][0].detach().numpy()
+    scores = softmax(scores)
+    scores_dict = {
+        'roberta_neg' : scores[0],
+        'roberta_neu' : scores[1],
+        'roberta_pos' : scores[2]
+    }
+    return scores_dict
+
+res = {}
+for i, row in df.iterrows():
+    try:
+        text = row['Text']
+        myid = row['Id']
+        vader_result = sia.polarity_scores(text)
+        vader_result_rename = {}
+        for key, value in vader_result.items():
+            vader_result_rename[f"vader_{key}"] = value
+        roberta_result = polarity_scores_roberta(text)
+        both = {**vader_result_rename, **roberta_result}
+        res[myid] = both
+    except RuntimeError:
+        print(f'Broke for id {myid}')
+
+results_df = pd.DataFrame(res).T
+results_df = results_df.reset_index().rename(columns={'index': 'Id'})
+results_df = results_df.merge(df, how='left')
+
+sns.pairplot(data=results_df,
+             vars=['vader_neg', 'vader_neu', 'vader_pos',
+                  'roberta_neg', 'roberta_neu', 'roberta_pos'],
+            hue='Score',
+            palette='tab10')
+plt.show()
